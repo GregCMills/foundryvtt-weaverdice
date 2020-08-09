07d4af57b42108a61b604fd4611b8bbe82e97d8a
Greg <gregscommunication@gmail.com>
2020-08-09 10:59:28 +1000

    comment (to understand) actor.js

--- esmodules/actor.js
+++ esmodules/actor.js
@@ -1,5 +1,6 @@
 /**
- * Extend the base Actor entity
+ * Extend the base Actor entity by defining a custom roll data structure which is ideal for the Simple system (will probably need to change to something better
+ * for weaverdice later).
  * @extends {Actor}
  */
 export class SimpleActor extends Actor {
@@ -10,25 +11,29 @@ export class SimpleActor extends Actor {
 		const data = super.getRollData();
 
 		// Re-map all attributes onto the base roll data
-		{
 		for ( let [k, v] of Object.entries(data.attributes) ) {
 			if ( !(k in data) ) data[k] = v.value;
 		}
 		delete data.attributes;
-		}
 
 		// Map all items data using their slugified names
-		data.items = this.data.items.reduce((obj, i) => {
-		let key = i.name.slugify({strict: true});
-		let itemData = duplicate(i.data);
-		{
+		// perform the custom reduce function on all elements of data.items
+		data.items = this.data.items.reduce((reducedItems, i) => {
+			// slugify (make human readable) item name
+			let key = i.name.slugify({strict: true});
+			// straight copy over the item data.
+			let itemData = duplicate(i.data);
+
+			// for each of the key value pairs in itemData.attributes.
 			for ( let [k, v] of Object.entries(itemData.attributes) ) {
-			if ( !(k in itemData) ) itemData[k] = v.value;
+				// If the attribute name isn't already directly in itemdata (i.e. is still in the .attribute sub property.), add it.
+				if ( !(k in itemData) ) itemData[k] = v.value;
 			}
+			// Once you've copied accross all the attributes directly to the itemdata delete the attribute property
 			delete itemData["attributes"];
-		}
-		obj[key] = itemData;
-		return obj;
+			//add the key-value pair of this item to the reducedItems list (or array or whatever, fuck javascript).
+			reducedItems[key] = itemData;
+			return reducedItems;
 		}, {});
 		return data;
     }
